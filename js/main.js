dedfb565eafdf3afde6e4c839ee66ec8efe780a0
Bence Kiss <kiss.bence.balint@gmail.com>
2017-02-15 16:39:45 -0500

    Added error class to inputs when clicked on submit.

--- js/main.js
+++ js/main.js
@@ -5,4 +5,7 @@ $(function(){
   $('.close').on('click', function(){
     $('.modal').fadeOut('slow');
   });
+  $('.submit').on('click', function(){
+    $('.getstarted input').addClass('error');
+  });
 });
